bf4d4c8ae508b0df8deed9e2fb9b55b98ea5c3a6
Petri Airio <petri.airio@valamis.com>
2019-07-01 08:43:27 +0300

    Test shutdown.

---
+++ pmud-client/src/main/java/org/freakz/pmud/pmudclient/PMudClientApplication.java
@@ -0,0 +1,32 @@
+package org.freakz.pmud.pmudclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+@Slf4j
+public class PMudClientApplication {
+
+    public static void main(String[] args) {
+
+        log.debug("Start ...");
+        ConfigurableApplicationContext ctx = new
+                SpringApplicationBuilder(PMudClientApplication.class).web(WebApplicationType.NONE).run(args);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.debug("Shutdown Hook is running !");
+            ctx.close();
+            System.exit(0);
+        }
+        ));
+
+        log.debug("... Hooking done!");
+
+    }
+
+}
